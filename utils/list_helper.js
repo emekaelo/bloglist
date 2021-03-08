d5e94b82e76a10b974faa4fe9612b42e8178f5fa
bruno <elochux@gmail.com>
2021-03-08 17:37:39 +0100

    Exercise 4.6*: helper functions and unit tests, step4 completed

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -12,21 +12,43 @@ const totalLikes = (blogs) => {
 // Returns the post with most likes
 const favoriteBlog = (blogs) => {
   let mostLikes = blogs[0].likes;
-  let favoriteBlog = blogs[0];
+  let favBlog = blogs[0];
   for (let i = 0; i < blogs.length; i++) {
     if (mostLikes < blogs[i].likes) {
       mostLikes = blogs[i].likes;
-      favoriteBlog = blogs[i];
+      favBlog = blogs[i];
     }
   }
-  delete favoriteBlog._id;
-  delete favoriteBlog.url;
-  delete favoriteBlog.__v;
-  return favoriteBlog;
+  delete favBlog._id;
+  delete favBlog.url;
+  delete favBlog.__v;
+  return favBlog;
+};
+
+// Returns the author who has the largest amount of blogs
+const mostBlogs = (blogs) => {
+  // Define the output format
+  let mostNumOfBlogs = {
+    author: "",
+    blogs: 0,
+  };
+  // Map out authors into an array
+  const authors = blogs.map((blog) => blog.author);
+
+  // Loop through & filter each author to find the number of times it appears
+  for (const author of authors) {
+    numOfAuthors = authors.filter((item) => item === author).length;
+    if (mostNumOfBlogs.blogs < numOfAuthors) {
+      mostNumOfBlogs.author = author;
+      mostNumOfBlogs.blogs = numOfAuthors;
+    }
+  }
+  return mostNumOfBlogs;
 };
 
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
 };

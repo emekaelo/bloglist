ec5e0738cc9636d5d55913352bab22c56c0be1e8
emekaelo <devemekaelo@gmail.com>
2024-04-17 05:48:18 +0100

    add webpack

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -1,53 +1,73 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const path = require('path');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const sass = require('sass')
+const webpack = require('webpack')
 
-module.exports = {
-  entry: "./index.js",
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js',
-    publicPath: '/'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader",
-          },
-        ],
-      },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: "style-loader",
-          },
-          {
-            loader: "css-loader",
-          },
-        ],
-      }
+module.exports = (env, argv) => {
+  const { mode } = argv
+  const additionalPlugins = mode === 'production'
+    ? []
+    : [new webpack.HotModuleReplacementPlugin()] // Enable hot module replacement
+
+  const additionalEntries = mode === 'production' ? [] : ['webpack-hot-middleware/client?http://localhost:8000']
+
+  return {
+    mode,
+    entry: [
+      '@babel/polyfill', // so we don't need to import it anywhere
+      './client/src',
+      ...additionalEntries,
     ],
-  },
-  resolve: {
-    extensions: ["*", ".js", ".jsx"],
-  },
-  devServer: {
-    historyApiFallback: true,
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "./public/index.html",
-      filename: "./index.html",
-    }),
-  ],
-};
+    resolve: {
+      alias: {
+        Components: path.resolve(__dirname, 'client/src/components/'),
+        '@root': path.resolve(__dirname),
+      },
+    },
+    module: {
+      rules: [
+        {
+          // Load JS files
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
+        },
+        { // Load SCSS & SASS files
+          test: /\.s[ac]ss$/i,
+          use: [
+            // Creates `style` nodes from JS strings
+            'style-loader',
+            // Translates CSS into CommonJS
+            'css-loader',
+            // Compiles Sass to CSS
+            {
+              loader: 'sass-loader',
+              options: {
+                // Prefer `dart-sass`
+                implementation: sass,
+              },
+            },
+          ],
+        },
+        {
+          // Load other files
+          test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+          use: ['file-loader'],
+        },
+      ],
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env.BUILT_AT': JSON.stringify(new Date().toISOString()),
+        'process.env.NODE_ENV': JSON.stringify(mode),
+      }),
+      // Skip the part where we would make a html template
+      new HtmlWebpackPlugin({
+        template: 'index.html'
+      }),
+      ...additionalPlugins,
+    ],
+  }
+}

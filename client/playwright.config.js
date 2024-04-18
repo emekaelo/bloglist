09a286a6e7a72920e926c726df146d364b110b7b
emekaelo <devemekaelo@gmail.com>
2024-04-18 22:03:23 +0100

    github action update

--- client/playwright.config.js
+++ client/playwright.config.js
@@ -74,7 +74,8 @@ module.exports = defineConfig({
     command: 'npm run start-prod',
     url: 'http://localhost:3001/',
     reuseExistingServer: !process.env.CI,
-    timeout: 2 * 60 * 1000
+    timeout: 2 * 60 * 1000,
+    port: 3001
   },
 })
 
